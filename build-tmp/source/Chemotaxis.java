e5e98f4543ad1914396b8e94269a5907379f66be
MostExcellent <dwglover3@gmail.com>
2014-10-01 11:46:32 -0700

    Chemotaxis Works!

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 ArrayList<Bacteria> bacteria = new ArrayList<Bacteria>();
 ArrayList<Food> food = new ArrayList<Food>();
-void setup()
+public void setup()
 {
 	for (int i = 0; i < 25; i++) {
 		bacteria.add(new Bacteria());
@@ -9,7 +25,7 @@ void setup()
 	size(500,500);
 	noStroke();
 }
-void draw()
+public void draw()
 {
 	background(10);
 	for (int i = 0; i < bacteria.size(); ++i) {
@@ -36,7 +52,7 @@ class Bacteria
 			myColor[i] = (int)(Math.random()*256);
 		}
 	}
-	void move(){
+	public void move(){
 		int xmove = (int)(Math.random()*10-5);
 		int ymove = (int)(Math.random()*10-5);
 		if(nfx-x < 0)
@@ -68,12 +84,12 @@ class Bacteria
 		x += xmove;
 		y += ymove;
 	}
-	void show()
+	public void show()
 	{
 		fill(myColor[0],myColor[1],myColor[2]);
 		ellipse(x, y, 10, 10);
 	}
-	void tick(){
+	public void tick(){
 		getNearestFood(food);
 		move();
 		show();
@@ -85,11 +101,11 @@ class Bacteria
 			food.add(new Food());
 		}
 	}
-	boolean isDead()
+	public boolean isDead()
 	{
 		return kcal <= 0;
 	}
-	void getNearestFood(ArrayList<Food> allFood)
+	public void getNearestFood(ArrayList<Food> allFood)
 	{
 		nfi=0;
 		int mindist = 500;
@@ -118,22 +134,31 @@ class Food
 		nc = (int)(Math.random()*100+100);
 	}
 
-	void show()
+	public void show()
 	{
 		fill(115, 90, 40);
 		ellipse(x, y, (int)(Math.random()*(nc/100)+4), (int)(Math.random()*(nc/100)+4));
 	}
 
-	int getX()
+	public int getX()
 	{
 		return x;
 	}
-	int getY()
+	public int getY()
 	{
 		return y;
 	}
-	int getNC(){
+	public int getNC(){
 		return nc;
 	}
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
